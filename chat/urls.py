ddc20daaeca5d16329a03ea0f7a7943fd74e1266
Mahmudul Alam <mahmudula2000@gmail.com>
2021-11-18 08:05:59 +0600

    Solved wrong wss protocol

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('connection-test', views.connection_test, name='connection-test'),
+    path('connection-test/', views.connection_test, name='connection-test'),
 ]
